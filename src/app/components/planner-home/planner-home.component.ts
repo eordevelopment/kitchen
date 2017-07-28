441f7574c4a6b4f913274c67014b298bf38c8648
eordevelopment <eordevelopment@gmail.com>
2017-07-28 16:04:02 +0100

    Dev 02 (#4)
    * Added momentjs
    
    * Prod build
    
    * start of planner section
    
    * Prod build
    
    * dummy planner page

--- src/app/components/planner-home/planner-home.component.ts
+++ src/app/components/planner-home/planner-home.component.ts
@@ -1,3 +1,4 @@
+import * as moment from 'moment';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./planner-home.component.less']
 })
 export class PlannerHomeComponent implements OnInit {
+  public days: moment.Moment[];
 
   constructor() { }
 
   ngOnInit() {
+    this.days = new Array();
+
+    const now = moment();
+
+    for (let i = 0; i < 7; i++) {
+      const dt = moment(now);
+      this.days.push(dt.add(i, 'd'));
+    }
   }
 
 }
