ee42d41428ffa29607f03322ee5d63cd4df5f146
eoriordan <eordevelopment@gmail.com>
2017-08-04 15:48:51 +0100

    Removed hardcoded recipe types

--- src/app/modules/recipe/pipes/recipe-type-filter.pipe.ts
+++ src/app/modules/recipe/pipes/recipe-type-filter.pipe.ts
@@ -1,9 +1,13 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import { IRecipe } from 'app/contract/IRecipe';
+import { IRecipeType } from 'app/contract/IRecipeType';
 
-@Pipe({name: 'recipeTypeFilter'})
+@Pipe({ name: 'recipeTypeFilter' })
 export class RecipeTypeFilterPipe implements PipeTransform {
-  transform(value: IRecipe[], type: number): IRecipe[] {
-    return value.filter(x => x.type === type);
+  transform(value: IRecipe[], type: IRecipeType): IRecipe[] {
+    if (!value) {
+      return undefined;
+    }
+    return value.filter(x => x.recipeType.id === type.id);
   }
 }
--- src/app/services/recipes.service.ts
+++ src/app/services/recipes.service.ts
@@ -11,6 +11,7 @@ import { StorageService } from './storage.service';
 import { ServiceError } from 'app/classes/error';
 import { environment } from '../../environments/environment';
 import { IRecipe } from 'app/contract/IRecipe';
+import { IRecipeType } from 'app/contract/IRecipeType';
 
 @Injectable()
 export class RecipesService {
@@ -59,6 +60,28 @@ export class RecipesService {
       .catch(this.handleError);
   }
 
+  public getRecipeType(id: number): Observable<IRecipeType> {
+    if (id <= 0) {
+      return Observable.of<IRecipeType>(null)
+    } else {
+      const headers = new Headers({ 'Authorization': `Basic ${this.storageService.getToken()}` });
+      const options = new RequestOptions({ headers: headers });
+
+      return this.http.get(environment.serviceUrl + 'recipetype/' + id, options)
+        .map(response => response.json() as IRecipeType)
+        .catch(this.handleError);
+    }
+  }
+
+  public getRecipieTypes(): Observable<IRecipeType[]> {
+    const headers = new Headers({ 'Authorization': `Basic ${this.storageService.getToken()}` });
+    const options = new RequestOptions({ headers: headers });
+
+    return this.http.get(environment.serviceUrl + 'recipetype', options)
+      .map(response => response.json() as IRecipeType[])
+      .catch(this.handleError);
+  }
+
   public saveRecipe(value: IRecipe): Observable<number> {
     const headers = new Headers({ 'Content-Type': 'application/json' });
     headers.append('Authorization', `Basic ${this.storageService.getToken()}`);
@@ -70,6 +93,22 @@ export class RecipesService {
         .catch(this.handleError);
     } else {
       return this.http.post(environment.serviceUrl + 'recipe', value, options)
+        .map(this.extractData)
+        .catch(this.handleError);
+    }
+  }
+
+  public saveRecipeType(value: IRecipeType): Observable<number> {
+    const headers = new Headers({ 'Content-Type': 'application/json' });
+    headers.append('Authorization', `Basic ${this.storageService.getToken()}`);
+    const options = new RequestOptions({ headers: headers });
+
+    if (value.id && value.id > 0) {
+      return this.http.put(environment.serviceUrl + 'recipetype/' + value.id, value, options)
+        .map(this.extractData)
+        .catch(this.handleError);
+    } else {
+      return this.http.post(environment.serviceUrl + 'recipetype', value, options)
         .map(this.extractData)
         .catch(this.handleError);
     }
